088add795cb1e9c32a5f4e795451d53005c6c66e
MitchellMudd <mitchellmudd@outlook.com>
2021-09-07 14:47:36 -0400

    Added image and font loaders

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,15 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ]
     }
 };
