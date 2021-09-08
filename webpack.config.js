0f479a7874db01b1382df1bf2a86bfba72f9fc79
Mitchell Mudd <mitchellmudd@outlook.com>
2021-09-08 18:04:28 +0000

    Tried playing around with settings to get webpack server to work and also started importing pictures so they can be put on the webpage

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        static: './dist',
+        compress: true,
+        host: '0.0.0.0',
+        port: 8080,
+        proxy: [{
+            target: 'http://localhost:3000',
+        }]
+    },
     mode: 'development',
     module: {
         rules: [
