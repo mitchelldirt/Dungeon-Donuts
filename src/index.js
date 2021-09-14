c137feff171490f0605d2961ae34d1fed8ae79e4
Mitchell Mudd <mitchellmudd@outlook.com>
2021-09-14 01:06:27 +0000

    attept to fix css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './styles.css';
 import choco from './imgs/choco.jpg';
 import d20 from './imgs/d20.png';
 import map from './imgs/map-background.png';
 import pow from './imgs/pow.jpg';
 import dragon from './imgs/drag.jpg';
+import './styles.css';
 console.log("We're in business o.0")
