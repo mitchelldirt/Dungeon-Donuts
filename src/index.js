2d95d5fe50739b2cd3360910911c46011f09ebbe
Mitchell Mudd <mitchellmudd@outlook.com>
2021-09-16 20:26:05 -0400

    Added some new images for the donuts and took out the older ones.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import choco from './imgs/choco.jpg';
+import tavern from './imgs/tavern.jpg';
 import d20 from './imgs/d20.png';
 import map from './imgs/map-background.png';
-import pow from './imgs/pow.jpg';
+import goblin from './imgs/goblin.png';
 import dragon from './imgs/drag.jpg';
+import mimic from './imgs/mimic.png';
+import beholder from './imgs/beholder.png';
 import './styles.css';
 console.log("We're in business o.0")
